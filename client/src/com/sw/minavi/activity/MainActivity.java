caf9dfe79210c9274099d8a883a97236eaeb0c57
siossan <love_kojopi@hotmail.co.jp>
2014-07-11 12:12:41 +0900

    talk karizissou

--- client/src/com/sw/minavi/activity/MainActivity.java
+++ client/src/com/sw/minavi/activity/MainActivity.java
@@ -1,27 +1,49 @@
 package com.sw.minavi.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-import com.sw.hkdwapp.R;
+import com.sw.minavi.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		/*
+		ImageView imageTalkIcon = (ImageView)findViewById(R.id.talk_icon);
+		imageTalkIcon.setOnClickListener(new OnClickListener() {
+		    public void onClick(View v) {
+		        Intent intent = new Intent();
+		        intent.setClassName("com.sw.minavi.activity", ".activity.TalkActivity");
+		        startActivity(intent);
+		    }
+		});
+		*/
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+
 		return true;
 	}
 
-	public void sonClickTalkBtn(){
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		if (v.getId() == R.id.talk_icon) {
+			Intent intent = new Intent(this, TalkActivity.class);
+			startActivity(intent);
+		}
 
 	}
 
--- client/src/com/sw/minavi/activity/TalkActivity.java
+++ client/src/com/sw/minavi/activity/TalkActivity.java
@@ -2,26 +2,26 @@ package com.sw.minavi.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-import com.sw.hkdwapp.R;
+import com.sw.minavi.R;
 
-public class TalkActivity extends Activity {
+public class TalkActivity extends Activity implements OnClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_talk);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	public void showTalkScreen(){
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+		if(v.getId() == R.id.back_area){
+			finish();
+		}
 		
 	}
 
